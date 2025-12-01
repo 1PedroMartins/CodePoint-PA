6fe3f21a80b841490da563ee451129f154857163
Pedro Martins <pedrofmartins2004@gmail.com>
2025-12-01 17:49:49 +0000

    correção minima

--- backend/routes/campanha.route.js
+++ backend/routes/campanha.route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/criarCampanha', campanhaController.createCampanha);
 router.get('/:id', campanhaController.getCampanha);
-router.get('/estado/:estado', campanhaController.getCampanhasEstado);
+router.get('/estado/:estado', campanhaController.getCampanhasPorEstado);
 router.patch('/:id', campanhaController.updateCampanha);
 router.delete('/:id', campanhaController.deleteCampanha);
 
